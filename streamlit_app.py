2a493d5154d124643b9e0d54d2627b4437f48f3a
Vasanth202514 <sk.vasa92@gmail.com>
2025-12-28 19:12:49 +0530

    Limit ingredient selection and modify dataframe display
    Updated multiselect to limit ingredient choices and commented out dataframe display.

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,15 +28,17 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit
 # st.stop()
 
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+# st.dataframe(pd_df)
+# st.stop()
 
-ingredient_list = st.multiselect('choose upto 5 ingredients:',my_dataframe)
+ingredient_list = st.multiselect('choose upto 5 ingredients:',my_dataframe,max_selections=5)
 ingredients_string =''
 if ingredient_list:   
 
   for fruit_chosen in ingredient_list:
     ingredients_string += fruit_chosen + ' '
+    search_on = pd_df.loc[pd_df['FRUIT_NAME'] ==  fruit_chosen,'SEARCH_ON'].iloc[0]
+    st.write('The search value for fruit chosen',fruit_chosen,' is ',search_on,'.')
     st.subheader(fruit_chosen + ' Nutrition Information')
     smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
     sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width=True)
