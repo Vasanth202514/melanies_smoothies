2db0260d684ddd7df28e1205e1d32397984169cf
Vasanth202514 <sk.vasa92@gmail.com>
2025-12-28 18:17:38 +0530

    Refactor requests import and API call in streamlit_app

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,12 +42,14 @@ my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_ord
 
 time_to_insert = st.button("Submit Order")
 
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response.json())
+
 if time_to_insert:
     session.sql(my_insert_stmt).collect()
     st.success(f"Your Smoothie is ordered, {name_on_order}!", icon="✅")
 
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+
 
 
