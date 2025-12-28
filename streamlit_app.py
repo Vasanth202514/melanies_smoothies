8caf4da1b4f17d7ae5e32e748fd4f50af815aa0c
Vasanth202514 <sk.vasa92@gmail.com>
2025-12-28 18:57:30 +0530

    Include 'search_on' in dataframe and display it
    Updated dataframe selection to include 'search_on' column and displayed it in the Streamlit app.

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,8 +23,9 @@ st.write("The Name on your Smoothie will be:",name_on_order)
 # st.write("Your favourite fruit is:",option)
 
 #session = get_active_session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'),col('search_on'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 ingredient_list = st.multiselect('choose upto 5 ingredients:',my_dataframe)
 ingredients_string =''
